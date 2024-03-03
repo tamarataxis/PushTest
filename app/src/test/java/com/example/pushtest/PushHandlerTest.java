f5a67f3050a5c4c28af474f1c9717de6702d7ea6
Tamara Taxis <tamara.taxis@gmail.com>
2024-03-03 20:46:32 +0100

    Add unit tests for edge cases when PushData or PushData members are null

--- app/src/test/java/com/example/pushtest/PushHandlerTest.java
+++ app/src/test/java/com/example/pushtest/PushHandlerTest.java
@@ -41,6 +41,35 @@ public class PushHandlerTest {
         assertFalse(result);
     }
 
+    @Test  // TODO: Null values are not handled! Use @NotNull annotation and delete test?
+    public void givenNullPushData_handlePush_ReturnsFalse() {
+        // given
+        PushHandler handler = new PushHandler(mStorageManager);
+        PushData data = null;
+
+        // when
+        Boolean result = handler.handlePush(data);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenNullUserLoginPushData_handlePush_ReturnsFalse() {
+        // given
+        PushHandler handler = new PushHandler(mStorageManager);
+        PushData data = new TestPushDataBuilderImpl()
+                .setValidDefaultData(PushType.Login)
+                .setUsername(null)
+                .build();
+
+        // when
+        Boolean result = handler.handlePush(data);
+
+        // then
+        assertFalse(result);
+    }
+
     @Test
     public void givenValidTransactionPushData_handlePush_ReturnsTrue() {
         // given
@@ -72,6 +101,22 @@ public class PushHandlerTest {
         assertFalse(result);
     }
 
+    @Test // TODO: Null values are not handled! Use @NotNull annotation and delete test?
+    public void givenNullTransactionSerialNumber_handlePush_ReturnsFalse() {
+        // given
+        PushHandler handler = new PushHandler(mStorageManager);
+        PushData data = new TestPushDataBuilderImpl()
+                .setValidDefaultData(PushType.Transaction)
+                .setSerialNumber(null)
+                .build();
+
+        // when
+        Boolean result = handler.handlePush(data);
+
+        // then
+        assertFalse(result);
+    }
+
     @Test
     public void givenValidRegistrationPushData_handlePush_ReturnsTrue() {
         // given
@@ -94,6 +139,22 @@ public class PushHandlerTest {
         PushData data = new TestPushDataBuilderImpl()
                 .setValidDefaultData(PushType.Registration)
                 .setUsername("Invalid User")
+                .build();
+
+        // when
+        Boolean result = handler.handlePush(data);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenNullRegistrationUser_handlePush_ReturnsFalse() {
+        // given
+        PushHandler handler = new PushHandler(mStorageManager);
+        PushData data = new TestPushDataBuilderImpl()
+                .setValidDefaultData(PushType.Registration)
+                .setUsername(null)
                 .build();
 
         // when
