e8b84ebb30c7f40445f3c8bc8f5569b0da019bc9
Tamara Taxis <tamara.taxis@gmail.com>
2024-03-03 14:50:39 +0100

    Configure JUnit - add smoke test to check that framework is configured correctly

---
+++ app/src/test/java/com/example/pushtest/PushHandlerTest.java
@@ -0,0 +1,14 @@
+package com.example.pushtest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PushHandlerTest {
+
+    @Test
+    public void smokeTestJunit() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
--- app/src/test/java/com/example/pushtest/PushHandlerUnitTest.java
+++
@@ -1,5 +0,0 @@
-package com.example.pushtest;
-
-public class PushHandlerUnitTest {
-
-}
